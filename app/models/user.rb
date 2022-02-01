5bbf9b9036c58413f0b624448a575422896a1d5c
AbdullahAltheiab <a.altheiab@takamol.com.sa>
2022-02-02 00:06:26 +0300

    [add] user validations for password and employee number. [add] users routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  MAX_EMPLOYEE_NUMBER_LENGTH = 4
+  MINIMUM_PASSWORD_LENGTH = 6
+
+  has_secure_password
+  validates :password, length: { minimum: MINIMUM_PASSWORD_LENGTH }, if: -> { new_record? || !password.nil? }
+  validates :employee_number, presence: true, uniqueness: true, numericality: { only_integer: true },
+            length: {is: MAX_EMPLOYEE_NUMBER_LENGTH}
 end
