d1989d853adafe4a5bf347c72d5e57c798f04bc5
AbdullahAltheiab <a.altheiab@takamol.com.sa>
2022-02-06 18:55:35 +0300

    [update] schema with reservations table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_02_192702) do
+ActiveRecord::Schema.define(version: 2022_02_05_133115) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "reservations", force: :cascade do |t|
+    t.datetime "start_at"
+    t.datetime "end_at"
+    t.string "customer_name"
+    t.bigint "table_id"
+    t.integer "seats"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["table_id"], name: "index_reservations_on_table_id"
+  end
 
   create_table "tables", force: :cascade do |t|
     t.integer "number"
@@ -31,4 +42,5 @@ ActiveRecord::Schema.define(version: 2022_02_02_192702) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "reservations", "tables"
 end
