8e3fa540b508606c360042449f287744eb3aaae9
AbdullahAltheiab <a.altheiab@takamol.com.sa>
2022-02-06 18:56:18 +0300

    [add] relations to reservations in table model. [add] best match function in tables model

--- app/models/table.rb
+++ app/models/table.rb
@@ -2,7 +2,14 @@ class Table < ApplicationRecord
   MIN_NUMBER_OF_SEATS = 1
   MAX_NUMBER_OF_SEATS = 12
 
+  has_many :reservations
+
   validates :number_of_seats, presence: true, inclusion: { in: MIN_NUMBER_OF_SEATS..MAX_NUMBER_OF_SEATS,
                                                            message: 'Must Be Within 1 to 12' }
   validates :number, presence: true, numericality: { greater_than: 0 }
+
+
+  def self.best_match(seats)
+    where(number_of_seats: seats)
+  end
 end
