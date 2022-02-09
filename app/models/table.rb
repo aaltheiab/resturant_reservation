083f1824fc746463971a672b7bf7d4da6f25527a
AbdullahAltheiab <a.altheiab@takamol.com.sa>
2022-02-10 01:15:50 +0300

    [add] uniqueness validator to table.rb number field. [enhance] table.rb class function best_match to handle availability API.

--- app/models/table.rb
+++ app/models/table.rb
@@ -7,11 +7,55 @@ class Table < ApplicationRecord
   validates :number_of_seats, presence: true, inclusion: { in: MIN_NUMBER_OF_SEATS..MAX_NUMBER_OF_SEATS,
                                                            message: 'Must Be Within 1 to 12' }
   validates :number, presence: true, numericality: { greater_than: 0 }
-
+  validates :number, uniqueness: true
 
   def self.best_match(seats)
-    # TODO enhance algorithm to take care of the best options and return the time slots instead of table
-    where(["number_of_seats >= ?", "#{seats}"]).order(:number_of_seats).first()
+    tables = best_table(seats)
+    return [] unless tables
+
+    best_time_slots(tables)
+  end
+
+  def self.best_time_slots(tables)
+    result = []
+    tables.each do |table|
+
+      now = Time.zone.now
+      start_time = now > now.noon ? now : now.noon # set to 12 noon if we are currently in the morning
+
+      if table.has_reservations_today?
+        # TODO calculate rest of the day excluding today's reservations
+        rvs = table.reservations.order('start_at')
+      else
+        # No reservation for the rest of the day ? then return rest of the day
+        result << table.time_slot_payload("#{start_time.strftime("%I:%M %p")} - #{now.end_of_day.strftime("%I:%M %p")}")
+      end
+    end
+    result
+  end
+
+  def time_slot_payload(time)
+    {
+      id: id,
+      table_number: number,
+      number_of_seats: number_of_seats,
+      time_slot: time
+    }
+  end
+
+  def has_reservations_today?
+    reservations.where(start_at: Time.zone.now..Time.zone.now.end_of_day).exists?
+  end
+
+  def self.best_table(seats)
+    tables = where(["number_of_seats = ?", "#{seats}"])
+    return tables if tables.exists?
+
+    table_seats = where(["number_of_seats > ?", "#{seats}"]).order(:number_of_seats).first&.number_of_seats
+
+    return [] unless table_seats
+
+    where(["number_of_seats = ?", "#{table_seats}"])
   end
 
   def has_available_slot?(start_at, end_at)
